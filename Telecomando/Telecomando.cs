e8eae8a475e5af93e3f33b7cadbf6c22c2c61d6d
DomeManca <83721834+DomeManca@users.noreply.github.com>
2022-10-12 11:41:58 +0200

    correzione in classe

--- Telecomando/Telecomando.cs
+++ Telecomando/Telecomando.cs
@@ -1,100 +1,126 @@
 using System;
 using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Forms;
 
 namespace Telecomando
 {
-    internal class TV
+    public partial class Telecomando : Form
     {
-        private string produttore;
-        private string modello;
-        private int volume;
-        private bool stato;
-        private int canale;
-        private string segnale;
-
-        //produttore e modello
-        public void setProduttore(string nuovoProduttore)
+        private TV tv;
+        
+        public Telecomando()
         {
-            produttore = nuovoProduttore;
+            InitializeComponent();
+            tv = new TV();
+            Reset();
         }
-        public string getProduttore()
-        { 
-            return produttore; 
-        }
-        public void setModello(string nuovoModello)
+        public void Reset()
         {
-            modello = nuovoModello;
+            label10.Text = "";
+            textBox2.Text = "Modello";
+            textBox1.Text = "Produttore";
+            tv.setSegnale("Infrarossi");
         }
-        public string getModello()
+        public void Accensione()
         {
-            return modello;
+            label8.Text = "1";
+            label9.Text = "15";
+            numericUpDown2.Value = 15;
+            numericUpDown3.Value = 1;
         }
-        //canale
-        public void setCanale(int nuovoCanale)
+        private void button1_Click(object sender, EventArgs e)
         {
-            canale = nuovoCanale;
-        }
-        public int getCanale()
-        {
-            return canale;
-        }
-        //volume
-        public void setVolume(int nuovoVolume)
-        {
-            volume = nuovoVolume;
-        }
-        public int getVolume()
-        {
-            return volume;
-        }
-        //stato
-        public void accendi()
-        {
-            setStato(true);
-        }
-        public void spegni()
-        {
-            setStato(false);
-        }
-        private void setStato(bool nuovoStato)
-        {
-            stato = nuovoStato;
-        }
-        public bool getStato()
-        {
-            return stato;
-        }
-        public bool inverti()
-        {
-            setStato(!stato);
-            return stato;
-        }
-        //segnale
-        public void switchSegnale()
-        {
-            if (segnale == "Bluetooth")
+            if(tv.inverti()== true)
             {
-                setSegnale("Wi-Fi");
-            }
-            else if (segnale == "Wi-Fi")
-            {
-                setSegnale("Infrarossi");
+                tv.accendi();
+                Accensione();
+                pictureBox1.BackColor = Color.Red;
             }
             else
             {
-                setSegnale("Bluetooth");
+                tv.spegni();
+                pictureBox1.BackColor = Color.White;
             }
         }
-        public void setSegnale(string v)
+        private void button2_Click(object sender, EventArgs e)
         {
-            segnale = v;
+            if (tv.getStato() == true)
+            {
+                tv.switchSegnale();
+                label10.Text = tv.getSegnale();
+            }      
         }
-        public string getSegnale()
+        private void numericUpDown2_ValueChanged(object sender, EventArgs e)
         {
-            return segnale;
+            int n = Convert.ToInt32(numericUpDown2.Value);
+            if (tv.getStato() == true)
+            {
+                tv.setVolume(n);
+                label9.Text = Convert.ToString(tv.getVolume());
+            }
+        }
+        private void numericUpDown3_ValueChanged(object sender, EventArgs e)
+        {
+            int n = Convert.ToInt32(numericUpDown3.Value);
+            if (n == 0)
+            {
+                numericUpDown3.Value = 999;
+                n = 999;
+            }
+            if (n == 1000)
+            {
+                numericUpDown3.Value = 1;
+                n = 1;
+            }
+            if (tv.getStato() == true)
+            {
+                tv.setCanale(n);
+                label8.Text = Convert.ToString(tv.getCanale());
+            }
+        }
+        private void button3_Click(object sender, EventArgs e)
+        {
+            if (tv.getStato() == true)
+            {
+                if(label9.Text == "0")
+                {
+                    label9.Text = Convert.ToString(numericUpDown2.Value);
+                    numericUpDown2.Enabled = true;
+                }
+                else
+                {
+                    label9.Text = "0";
+                    numericUpDown2.Enabled = false;
+                }
+            }  
+        }
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            if (tv.getStato() == true)
+            {
+                tv.setProduttore(textBox1.Text);
+                label7.Text = tv.getProduttore();
+            }
+        }
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+            if (tv.getStato() == true)
+            {
+                tv.setModello(textBox2.Text);
+                label6.Text = tv.getModello();
+            }
+        }
+        private void button4_Click(object sender, EventArgs e)
+        {
+            if (tv.getStato() == false)
+                Reset();
+            MessageBox.Show("Reset avvenuto con successo!", "SUCCESSO");
         }
     }
 }
